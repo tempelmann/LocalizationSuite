0c1d91821d8b01b3a338adafa9f1333de0143c5d
Thomas Tempelmann <tempelmann@gmail.com>
2019-09-29 23:52:06 +0200

    Updated path to my "Localize Me" project

--- Manager/Source/Controllers/Controller.m
+++ Manager/Source/Controllers/Controller.m
@@ -37,7 +37,7 @@ id __sharedControllerInstance;
 	#if 0
 		// For faster debugging:
 		[[NSDocumentController sharedDocumentController] newDocument: self];
-		[BLXcodeImporter importXcodeProjectAtPath:@"/Users/tempi/Desktop/Localize Me/Localize Me.xcodeproj" toDatabaseDocument:[[NSDocumentController sharedDocumentController] currentDocument] withOptions:BLXcodeImporterRescanExistingFiles];
+		[BLXcodeImporter importXcodeProjectAtPath:@"/Applications/Develop Tools/Localization/Localization Suite github/• Localize Me/Localize Me.xcodeproj" toDatabaseDocument:[[NSDocumentController sharedDocumentController] currentDocument] withOptions:BLXcodeImporterRescanExistingFiles];
 	#endif
 }
 
